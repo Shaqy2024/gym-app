eed69436ed18bae772c855eb8e0521a1b4a67e0e
Shaqy2024 <sakib950006@gmail.com>
2025-02-10 22:25:26 +0530

    commit

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { config } from "dotenv";
-import cors from "cors";
+
 import { sendEmail } from "./utils/sendEmail.js";
 
 const app = express();
@@ -8,13 +8,7 @@ const router = express.Router();
 
 config({ path: "./config.env" });
 
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["POST"],
-    credentials: true,
-  })
-);
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
