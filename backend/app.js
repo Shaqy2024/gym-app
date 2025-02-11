52c91ddd8a215d4a3f590b0a3ed2a2a69fba2a48
Shaqy2024 <sakib950006@gmail.com>
2025-02-11 21:23:46 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,13 @@ const router = express.Router();
 
 config({ path: "./config.env" });
 
-app.use(cors());
+app.use(cors(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["POST"],
+    credentials: false,
+  })
+));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
